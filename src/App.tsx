e4f83b480081bc5567fb9d14475241b80f076874
Rudy <rudyyazdi@gmail.com>
2023-02-13 12:27:20 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,9 +61,11 @@ function App() {
   const [focusAreas, setFocusAreas] = useState<FocusArea[]>(
     DEFAULT_DATA.focusAreas
   );
+  const DRAGGABLE_TYPE = "BOX";
+
   const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
     // "type" is required. It is used by the "accept" specification of drop targets.
-    type: "BOX",
+    type: DRAGGABLE_TYPE,
     // The collect function utilizes a "monitor" instance (see the Overview for what this is)
     // to pull important pieces of state from the DnD system.
     collect: (monitor) => ({
@@ -73,48 +75,56 @@ function App() {
 
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
     // The type (or types) to accept - strings or symbols
-    accept: 'BOX',
+    accept: DRAGGABLE_TYPE,
     // Props to collect
     collect: (monitor) => ({
       isOver: monitor.isOver(),
-      canDrop: monitor.canDrop()
-    })
-  }))
+      canDrop: monitor.canDrop(),
+    }),
+  }));
 
   console.log({ isDragging });
 
   return (
     <div className="App">
       {/* for each focus area create a div with the name on the top and red border and then render
-      each objective with title underneath. Draw green border around each objective div */}
+      each objective with title underneath. Draw green border around each objective div. Both divs
+      have 10px margin and padding.
+
+      The objective divs should be draggable in a way that the user can change the order of the
+      objectives within the focus area. The focus area divs should be droppable.
+      */}
+
+      <div
+        style={{
+          border: "1px solid blue",
+          margin: "10px",
+          padding: "10px",
+        }}
+        ref={drag}
+        >
+        <h3>Draggable Area</h3>
+      </div>
+
       {focusAreas.map((focusArea) => (
         <div
-          key={focusArea.id}
+          ref={drop}
           style={{
             border: "1px solid red",
-            padding: "10px",
             margin: "10px",
-            backgroundColor: isOver ? 'red' : 'white'
+            padding: "10px",
           }}
-          ref={drop}
-          role={'Dustbin'}
         >
-          <h2>{focusArea.name}</h2>
-          {canDrop ? 'Release to drop' : 'Drag a box here'}
+          <h3>{focusArea.name}</h3>
           {focusArea.objectives.map((objective) => (
-            <div ref={dragPreview} style={{ opacity: isDragging ? 0.5 : 1 }}>
-              <div
-                key={objective.id}
-                style={{
-                  border: "1px solid green",
-                  padding: "10px",
-                  margin: "10px",
-                }}
-                ref={drag}
-                role="Handle"
-              >
-                <h3>{objective.name}</h3>
-              </div>
+            <div
+              style={{
+                border: "1px solid green",
+                margin: "10px",
+                padding: "10px",
+              }}
+            >
+              <h4>{objective.name}</h4>
             </div>
           ))}
         </div>
